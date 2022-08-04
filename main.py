88c6723376dbab1278a39e22946167e32461a05c
Уваров Илья <popodoss@mail.ru>
2022-08-04 15:18:27 +0300

    dz

--- main.py
+++ main.py
@@ -4,21 +4,31 @@ class Student:
         self.surname = surname
         self.gender = gender
         self.finished_courses = []
-        self.courses_in_progress = ['git', 'Пайтон для разработчиков']
+        self.courses_in_progress = []
         self.grades = {}
         self.courses_in_attack = ['Программировние на пайтон']
 
-    def __str__(self):
-        return (f"Student\nИмя: {self.name}\nФамилия: {self.surname}\nОбщая оценка: \nПройденные курсы"
-                f"{self.courses_in_attack}\nКурсы в процессе изучения{self.courses_in_progress}")
+    def gr_student(self):
+        sum_grade = 0
+        grafes = 0
+        for course in self.grades.values():
+            sum_grade += sum(course)
+            grafes += len(course)
+        return sum_grade / grafes
 
-    def rate_student(self, lecturer, course, grade):
+
+
+    def __str__(self):
+        return (f"Student\nИмя: {self.name}\nФамилия: {self.surname}\nОбщая оценка:{self.gr_student()} \nПройденные курсы"
+                f"{self.courses_in_attack}\nКурсы в процессе изучения{self.courses_in_progress}\n")
+
+    def rate_student(self, lecturer, course, lectors_grade):
         if isinstance(lecturer,
-                      lecturer) and course in self.courses_in_attack and course in lecturer.courses_in_progress:
-            if course in lecturer.gradess:
-                lecturer.gradess[course] += [grade]
+                      Lecturer) and course in self.courses_in_progress:
+            if course in lecturer.lectors_grade:
+                lecturer.lectors_grade[course] += [lectors_grade]
             else:
-                lecturer.gradess[course] = [grade]
+                lecturer.lectors_grade[course] = [lectors_grade]
         else:
             return 'Ошибка'
 
@@ -42,9 +52,9 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.name = name
         self.surname = surname
-        self.lectors_grade = {10, 9, 6}
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        self.lectors_grade = {}
+    def rate_hwe(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course :
             if course in student.grades:
                 student.grades[course] += [grade]
             else:
@@ -52,33 +62,51 @@ class Lecturer(Mentor):
         else:
             return 'Ошибка'
 
+    def sum_grades(self):
+        sum_grade = 0
+        col = 0
+        for course in self.lectors_grade.values():
+            sum_grade += sum(course)
+            col += len(course)
+        return sum_grade / col
+
     def __str__(self):
         nam = self.name
         sur = self.surname
-        return (f"Lecturer\nИмя: {nam}\nФамилия: {sur}\nОбщая оценка: ")
+        return (f"Lecturer\nИмя: {nam}\nФамилия: {sur}\nОбщая оценка: {self.sum_grades()}\n ")
 
-arkadieprepod = Lecturer("Аркадий", "Поровозов")
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
-        self.name = name
-        self.surname = surname
+
     def __str__(self):
         return (f"Reviewer \nИмя: {self.name} \nФамилия: {self.surname}")
 
 
-some_reviewer = Reviewer("Паша", "Техник")
-
-
-
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['git']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_Lecturer = Lecturer('Some', 'Buddy')
+cool_Lecturer.courses_attached += ['Python']
+cool_Lecturer.courses_attached += ['git']
 
+cool_Lecturer.rate_hwe(best_student, 'Python', 10)
+cool_Lecturer.rate_hwe(best_student, 'Python', 9)
+cool_Lecturer.rate_hwe(best_student, 'Python', 10)
 
-print(some_reviewer)
-print(onestuden)
-print(arkadieprepod)
+cool_Lecturer.rate_hwe(best_student, 'git', 5)
+cool_Lecturer.rate_hwe(best_student, 'git', 9)
+cool_Lecturer.rate_hwe(best_student, 'git', 10)
+
+best_student.rate_student(cool_Lecturer, 'git', 10)
+best_student.rate_student(cool_Lecturer, 'git', 6)
+best_student.rate_student(cool_Lecturer, 'git', 8)
+
+hew_revier = Reviewer('Джек', 'Воробей')
+
+print(best_student)
+print(cool_Lecturer)
+print(hew_revier)
