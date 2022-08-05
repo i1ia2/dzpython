4a9dff4efb24cfd0487620ee9cfaa5b690feaa27
Уваров Илья <popodoss@mail.ru>
2022-08-05 04:17:03 +0300

    dz

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Student:
 
     def __str__(self):
         return (f"Student\nИмя: {self.name}\nФамилия: {self.surname}\nОбщая оценка:{self.gr_student()} \nПройденные курсы"
-                f"{self.courses_in_attack}\nКурсы в процессе изучения{self.courses_in_progress}\n")
+                f"{self.courses_in_attack}\nКурсы в процессе изучения{self.courses_in_progress}")
 
     def rate_student(self, lecturer, course, lectors_grade):
         if isinstance(lecturer,
@@ -32,6 +32,9 @@ class Student:
         else:
             return 'Ошибка'
 
+    def __lt__(self, dark_student):
+        sravni = self.gr_student() < dark_student.gr_student()
+        return sravni
 
 onestuden = Student("Игорь", "Куращуп", "")
 
@@ -62,6 +65,8 @@ class Lecturer(Mentor):
         else:
             return 'Ошибка'
 
+
+
     def sum_grades(self):
         sum_grade = 0
         col = 0
@@ -70,12 +75,16 @@ class Lecturer(Mentor):
             col += len(course)
         return sum_grade / col
 
+
+
+    def __lt__(self, two_lektor):
+        sravnenie = self.sum_grades() < two_lektor.sum_grades()
+
+        return sravnenie
+
     def __str__(self):
-        nam = self.name
-        sur = self.surname
-        return (f"Lecturer\nИмя: {nam}\nФамилия: {sur}\nОбщая оценка: {self.sum_grades()}\n ")
-
-
+        nan = (f"Lecturer\nИмя: {self.name}\nФамилия: {self.surname}\nОбщая оценка: {self.sum_grades()}")
+        return nan
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -89,9 +98,17 @@ best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 best_student.courses_in_progress += ['git']
 
+dark_student = Student('Dart', 'Veider', 'your_gender')
+dark_student.courses_in_progress += ['Python']
+dark_student.courses_in_progress += ['git']
+
 cool_Lecturer = Lecturer('Some', 'Buddy')
 cool_Lecturer.courses_attached += ['Python']
 cool_Lecturer.courses_attached += ['git']
+
+best_student.rate_student(cool_Lecturer, 'git', 10)
+best_student.rate_student(cool_Lecturer, 'git', 6)
+best_student.rate_student(cool_Lecturer, 'git', 8)
 
 cool_Lecturer.rate_hwe(best_student, 'Python', 10)
 cool_Lecturer.rate_hwe(best_student, 'Python', 9)
@@ -101,12 +118,26 @@ cool_Lecturer.rate_hwe(best_student, 'git', 5)
 cool_Lecturer.rate_hwe(best_student, 'git', 9)
 cool_Lecturer.rate_hwe(best_student, 'git', 10)
 
-best_student.rate_student(cool_Lecturer, 'git', 10)
-best_student.rate_student(cool_Lecturer, 'git', 6)
-best_student.rate_student(cool_Lecturer, 'git', 8)
+cool_Lecturer.rate_hwe(dark_student, 'Python', 10)
+cool_Lecturer.rate_hwe(dark_student, 'Python', 9)
+cool_Lecturer.rate_hwe(dark_student, 'Python', 10)
+
+cool_Lecturer.rate_hwe(dark_student, 'git', 10)
+cool_Lecturer.rate_hwe(dark_student, 'git', 10)
+cool_Lecturer.rate_hwe(dark_student, 'git', 10)
+
+two_lektor = Lecturer('Somes', 'Buddyb')
+two_lektor.courses_attached += ['Python']
+two_lektor.courses_attached += ['git']
+
+best_student.rate_student(two_lektor, 'git', 10)
+best_student.rate_student(two_lektor, 'git', 5)
+best_student.rate_student(two_lektor, 'git', 8)
 
 hew_revier = Reviewer('Джек', 'Воробей')
 
-print(best_student)
-print(cool_Lecturer)
+print(f"{best_student}\nЛучший студент в аккадемии:{best_student > dark_student}\n")
+
+print(f'{cool_Lecturer}\nЛучший лектор в аккадемии:{cool_Lecturer > two_lektor}\n' )
+
 print(hew_revier)
